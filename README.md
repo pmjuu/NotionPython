37fab70d36371a01976130665c359f36bcf5aee2
Minju <50537876+pmjuu@users.noreply.github.com>
2021-06-22 21:32:41 +0900

    .

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Notion - Python API 활용
 참고자료 [Unofficial Python 3 client for Notion.so API v3](https://github.com/jamalex/notion-py)
+### 추가 설명
+MY_TOKEN 은 개별적으로 발급받으면 됩니다.   
+(크롬 개발자 도구 - Application - Cookies - token_v2의 Value값 복사)   
+보안상 문제 때문에 저는 .gitignore 처리한 `MY_TOKEN.txt`에 따로 저장해놓고 불러왔습니다.
+
 ### 트러블 슈팅 기록
-`can't connect to https url because the ssl module is not available.`   
-    => 시스템 환경 변수 편집 - path 추가
+SSL Error `can't connect to https url because the ssl module is not available.`   
+=> 시스템 환경 변수 편집 - path 추가
 ```
 C:\Anaconda3
 C:\Anaconda3\Scripts
--- autoCreate_plannerPage.py
+++ autoCreate_plannerPage.py
@@ -1,5 +1,7 @@
+import os
 from notion.client import *
 from notion.block import *
+import datetime as dt
 
 BASE_DIR = os.path.dirname(__file__)
 with open(os.path.join(BASE_DIR, 'MY_TOKEN.txt'), 'r') as f:
@@ -9,4 +11,9 @@ PAGE_URL = 'https://www.notion.so/Experiment-fcb88e28c7694a31a0e4897c59fc34e5'
 client = NotionClient(token_v2 = MY_TOKEN)
 page = client.get_block(PAGE_URL)
 
-newchild1 = page.children.add_new(TodoBlock, title='블록 자동생성')
+pTime = dt.datetime.now()
+page.children.add_new(PageBlock, title = pTime.strftime('%Y년 %m월 %d일 %H시 %M분 %S초'))
+
+for child in page.children:
+    child_page = client.get_block(child.id)
+    little_child = child_page.children.add_new(TodoBlock, title = pTime.strftime('%H시 %M분 %S초'))
